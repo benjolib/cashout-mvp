802ce3ec91ee2da6b1ca2259e266f65e834fcaa7
Stefan Walkner <stefan@walkner.biz>
2015-04-01 16:26:27 +0200

    initial commit

---
+++ Cash Out/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cash Out
+//
+//  Created by Stefan Walkner on 01.04.15.
+//  Copyright (c) 2015 Cashout App GbR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Cash Out/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Cash Out
+//
+//  Created by Stefan Walkner on 01.04.15.
+//  Copyright (c) 2015 Cashout App GbR. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
