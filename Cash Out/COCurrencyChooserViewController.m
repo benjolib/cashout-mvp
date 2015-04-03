1df395c014ef40d0309e575d7a945225c4b8ccf8
Stefan Walkner <stefan@walkner.biz>
2015-04-03 09:32:15 +0200

    cash view, new pods

--- Cash Out/COCurrencyChooserViewController.m
+++ Cash Out/COCurrencyChooserViewController.m
@@ -5,13 +5,89 @@
 
 #import "COCurrencyChooserViewController.h"
 
+@interface COCurrencyChooserViewController() <UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, strong) NSMutableArray *customConstraints;
+
+@end
+
+static NSString *CellIdentifier = @"COCurrencyChooserViewControllerCell";
 
 @implementation COCurrencyChooserViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    self.automaticallyAdjustsScrollViewInsets = NO;
+
+    _customConstraints = [[NSMutableArray alloc] init];
+
+    self.tableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain];
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    [self.view addSubview:self.tableView];
+
     self.view.backgroundColor = [UIColor whiteColor];
+
+    [self.view setNeedsUpdateConstraints];
+}
+
+- (void)updateViewConstraints {
+    [super updateViewConstraints];
+
+    NSDictionary *views = @{@"tableView":self.tableView};
+
+    for (UIView *view in views.allValues) {
+        view.translatesAutoresizingMaskIntoConstraints = NO;
+    }
+
+    [self.view removeConstraints:self.customConstraints];
+    [self.customConstraints removeAllObjects];
+
+    [self.customConstraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[tableView]|" options:NSLayoutFormatDirectionLeadingToTrailing metrics:nil views:views]];
+    [self.customConstraints addObject:[NSLayoutConstraint constraintWithItem:self.tableView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.topLayoutGuide attribute:NSLayoutAttributeBottom multiplier:1 constant:0]];
+    [self.customConstraints addObject:[NSLayoutConstraint constraintWithItem:self.tableView attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.bottomLayoutGuide attribute:NSLayoutAttributeTop multiplier:1 constant:0]];
+
+    [self.view addConstraints:self.customConstraints];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self currencies].count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+        cell.textLabel.textAlignment = NSTextAlignmentCenter;
+    }
+
+    cell.textLabel.text = self.currencies[(NSUInteger) indexPath.row];
+
+    return cell;
+}
+
+- (NSArray *)currencies {
+    return @[
+            @"USDCAD",
+            @"GPBUSD",
+            @"GBPJPY",
+            @"USDCHF",
+            @"NZDUSD",
+            @"EURJPY",
+            @"EURGBP",
+            @"EURUSD",
+            @"EURGBP",
+            @"DAX",
+            @"GOLD",
+            @"DOW",
+            @"CRUDE OIL",
+            @"NASDAQ",
+            @"SP500",
+            @"BITCOINS"
+    ];
 }
 
 @end
--- Podfile
+++ Podfile
@@ -2,3 +2,4 @@ platform :ios, '7.0'
 
 pod 'Realm', '0.91.1'
 pod 'AFNetworking', '2.5.2'
+pod 'SVProgressHUD', '1.1.3'
